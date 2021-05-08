63a8a6dd6c96dc4df7070b755fa106d2cb0113bc
unokun <unokun@gmail.com>
2021-05-08 09:25:56 +0900

    Initial Commit

---
+++ PopoverSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PopoverSample
+//
+//  Created by Masaaki Uno on 2021/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PopoverSample/PopoverSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopoverSampleApp.swift
+//  PopoverSample
+//
+//  Created by Masaaki Uno on 2021/05/08.
+//
+
+import SwiftUI
+
+@main
+struct PopoverSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
